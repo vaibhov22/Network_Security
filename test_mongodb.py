ffee1704ed2edf04e6ba34d760e6f81e17135b8c
vaibhov22 <sonivaibhov781@gmail.com>
2025-12-16 02:40:14 +0530

    second commit

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,8 +1,11 @@
 
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-uri = "mongodb+srv://vaibhov_soni:Admin123@cluster0.ibabgd2.mongodb.net/?appName=Cluster0"
+uri = os.getenv("Mongo_DB_URL")
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
